db5ad1e9d38156c8267c34336630d7d17a3a4193
Keshavjha96 <keshav.jha9625@gmail.com>
2026-06-23 23:21:04 +0530

    Update var.js

---
+++ variable/var.js
@@ -0,0 +1,11 @@
+const accountId = '1234567890';
+let email = "keshav@gmail.com";
+var accountPassword = 'password123';
+let accountCity = 'New York';
+
+accountPassword = 'newpassword456';
+email = "keshav.jha118892@gmail.com";
+accountCity = 'Los Angeles';
+
+console.log('Account ID:', accountId);
+console.table({ email, accountPassword, accountCity });
