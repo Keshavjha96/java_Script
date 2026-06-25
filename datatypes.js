7290777c76c1edfd2dd5f76d6ead5cefbbd0227c
Keshavjha96 <keshav.jha9625@gmail.com>
2026-06-25 20:11:58 +0530

    code

---
+++ datatypes.js
@@ -0,0 +1,8 @@
+let name="Keshav jha";
+let age=22;
+let isLoggedIn=true;
+let state=null;
+//number 2 to power 53
+//null =stand alone value
+//symbol=> unique identifier
+console.log(typeof name);
