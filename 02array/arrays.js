49d7adb70e6b6cab756e2299e3f04f1e23762e44
Keshavjha96 <keshav.jha9625@gmail.com>
2026-07-01 20:14:56 +0530

    code

---
+++ 02array/arrays.js
@@ -0,0 +1,32 @@
+// //array 
+
+const arr = [1, 2, 3, 4, 5];
+// const Myheroes = ['Ironman', 'Spiderman', 'Hulk', 'Thor', 'Captain America'];
+// console.log(arr);
+// console.log(Myheroes);  
+
+// //Array methods
+
+// arr.push(6);
+// console.log(arr);
+
+// // arr.unshift(0);
+// // console.log(arr);
+// // arr.shift();
+
+// console.log(arr.includes(3));
+// console.log(arr.indexOf(3));
+
+const newArr=arr.join();
+// console.log(newArr);
+
+console.log("A",arr)
+
+const myA1=arr.slice(1,3);
+console.log(myA1);
+
+console.log("B",arr);
+
+const myA2=arr.splice(1,3);
+console.log("c",arr);
+console.log(myA2);
