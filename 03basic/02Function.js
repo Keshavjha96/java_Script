f38a50d41f489e07930beb3f778944096ff08df8
Keshavjha96 <keshav.jha9625@gmail.com>
2026-07-02 21:40:17 +0530

    code

---
+++ 03basic/02Function.js
@@ -0,0 +1,12 @@
+function calculateCartPrice(val1,val2,...num1) {
+    return num1;
+}
+//nsole.log(calculateCartPrice(100,200,300,400,500));
+const user ={
+    username: "keshav",
+    price: 20,
+}
+function handleObject( anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+handleObject(user);
