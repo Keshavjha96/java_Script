eae4d985eeec603bdffdefd9acf06bd0d519b9fa
Keshavjha96 <keshav.jha9625@gmail.com>
2026-07-05 19:01:46 +0530

    code

---
+++ 04ControlFlow.js/switchcase.js
@@ -0,0 +1,21 @@
+const month=3
+
+switch (month) {
+    case 1:
+        console.log("january")
+        break;
+    case 2:
+        console.log("feb")
+        break;
+
+    case 3:
+        console.log("March")
+        break;
+    case 2:
+        console.log("April")
+        break;
+
+    default:
+        console.log("defalut case match")
+        break;
+}
