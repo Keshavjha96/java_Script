818e6475305943b7687b645a6f23c452aaa3a14c
Keshavjha96 <keshav.jha9625@gmail.com>
2026-07-06 23:11:29 +0530

    code

---
+++ iterations.js/five.js
@@ -0,0 +1,18 @@
+ const coading=["js","java","c++","python","ruby"]
+// coading.forEach(function (val){
+//     console.log(val);
+// })
+
+// coading.forEach( (item)=>{
+//     console.log(item);
+// })
+
+// function printMe(item){
+//     console.log(item);
+// }
+
+// coading.forEach(printMe)
+
+coading.forEach( (item ,index,arr)=>{
+    console.log(item,index,arr);
+})
