342c659e54c5c93f38f983d2d047e1545deaeab3
Keshavjha96 <keshav.jha9625@gmail.com>
2026-07-06 23:00:59 +0530

    code

---
+++ iterations.js/object.js
@@ -0,0 +1,15 @@
+const myObject={
+    js:'javascript',
+    cpp:"c++",
+    rb:"ruby",
+    swift:"swift by apple",
+}
+
+for (const key in myObject) {
+    console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+const programing=["js","c++","ruby","swift"]
+for (const key in programing) {
+    console.log(programing[key]);
+}
