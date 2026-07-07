df1a2642c6028d13063ca9c908a33d814fdd0236
Keshavjha96 <keshav.jha9625@gmail.com>
2026-07-07 18:37:10 +0530

    code

---
+++ iterations.js/six.js
@@ -0,0 +1,24 @@
+ const coading=["js","java","c++","python","ruby"]
+
+ const values=coading.forEach((item)=>{
+   // console.log(item);
+   // return item
+ })
+ //console.log(values);
+
+ const myNums=[1,2,3,4,5,6,7,8.9,10]
+
+//  const newnums=myNums.filter((num)=>{
+//    return num>4
+// })
+//  console.log(newnums);  
+
+const newnums=[]
+
+// myNums.forEach((num)=>{
+// if (num>4) {
+//     newnums.push(num)
+// }
+// })
+// console.log(newnums);
+
