a727ea6fe7b5d750a98cf2cdb26dc2cfc0d92e00
Keshavjha96 <keshav.jha9625@gmail.com>
2026-09-01 21:11:03 +0530

    code

---
+++ variable/stringg.js
@@ -0,0 +1,23 @@
+const name="Keshav";
+const repoCount=50;
+console.log(`My name is ${name} and I have ${repoCount} repositories on GitHub.`);
+
+const gamename="free-fire-op";
+console.log(gamename.charAt(0));
+console.log(gamename.indexOf("f"));
+
+const newString=gamename.substring(0,4);
+console.log(newString);
+
+const newString2=gamename.slice(0,4);
+console.log(newString2);
+
+const newStirngOne ="    Keshav jha  ";
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url="https://www.youtube.com/watch?v%20=Jg0X7k1b5xA";
+ console.log(url.replace('%20','_'));
+ console.log(url.includes('youtube'));
+
+ console.log(gameName.split('-'));
